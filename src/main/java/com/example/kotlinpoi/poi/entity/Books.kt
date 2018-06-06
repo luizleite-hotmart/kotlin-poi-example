83c85fef947261948546906eac060477d085f7e3
luizleite <luiz.leite@hotmart.com.br>
2018-06-05 21:12:18 -0300

    add class books

---
+++ src/main/java/com/example/kotlinpoi/poi/entity/Books.kt
@@ -0,0 +1,27 @@
+package com.example.kotlinpoi.poi.entity
+
+import java.util.Date
+
+class Books {
+    var id: Long? = null
+    var author: String? = null
+    var name: String? = null
+    var releaseDate: Date? = null
+    var numerOfPages: Int = 1
+    var description: String? = null
+
+    constructor() {}
+    constructor(id: Long?, author: String?, name: String?, releaseDate: Date?, numerOfPages: Int, description: String?) {
+        this.id = id
+        this.author = author
+        this.name = name
+        this.releaseDate = releaseDate
+        this.numerOfPages = numerOfPages
+        this.description = description
+    }
+
+    override fun toString(): String {
+        return "Book [id = ${id}, author = ${author}, releaseDate ${releaseDate}, numberOfPages = ${numerOfPages}, " +
+                "description = ${description} ]"
+    }
+}
