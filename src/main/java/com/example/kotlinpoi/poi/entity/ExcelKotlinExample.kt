63c2f4696f1f05566d807c2d6f6213c3f1b9f297
luizleite <luiz.leite@hotmart.com.br>
2018-06-05 21:36:55 -0300

    format cells

--- src/main/java/com/example/kotlinpoi/poi/entity/ExcelKotlinExample.kt
+++ src/main/java/com/example/kotlinpoi/poi/entity/ExcelKotlinExample.kt
@@ -1,5 +1,7 @@
 package com.example.kotlinpoi.poi.entity
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.IOException
 import java.util.*
 
 private val COLUMNS = arrayOf<String>( "Id", "Author", "Release Date", "Number of Pages", "Description")
@@ -7,6 +9,16 @@ private val books = Arrays.asList(
         Book(2518L, "Will Shake", "Rom e Ju", Date(-2197741789000), 254, "Any coincidence is just a coincidence"),
         Book(58412L, "Paul rabbit", "Alu", Date(1320981011000), 10, "Brief what is the book"),
         Book(8105L, "Peter Cabral", "Brazil the discover", Date(-15204397789000), 15, "Brazil a big forest"),
-        Book(8158L, "Luiz Leite Oliveira", "Kotlin + poi", Date(1528244651862), 25, "How to use POI on kotlin")
+        Book(8158L, "Luiz Leite Oliveira", "Kotlin + poi", Date(1528244651862), 25, "How to use POI on kotlin"))
 
-)
+
+@Throws(IOException::class)
+fun main(args: Array<String>?) {
+    val workbook = XSSFWorkbook()
+    val create = workbook.creationHelper
+    val sheet = workbook.createSheet("Books")
+    val headerFont = workbook.createFont()
+    headerFont.bold = true
+    val headerCellStyle = workbook.createCellStyle()
+
+}
